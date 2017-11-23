5b9080026bf57826640e9d2a07e1d0e9367f930c
Valiya Rakkanchath (Karthik) <RakkancA@advisory.com>
2017-11-23 12:09:22 +0530

    ADDED initial server scaffold

---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import express from 'express';
+import morgan from 'morgan';
+import favicon from 'serve-favicon';
+import methodOverride from 'method-override';
+import compression from 'compression';
+import path from 'path';
+import logger from './lib/services/logger';
+import middleware from './lib/middleware';
+
+const buildDir = path.join(__dirname, 'public', 'assets');
+const port = 3000;
+const app = express();
+
+app.use(morgan('dev', {
+  stream: logger.stream
+}));
+app.use(compression());
+app.use(methodOverride());
+app.use(middleware.setNoCache);
+app.use(middleware.encodedBodyParser);
+app.use(middleware.jsonBodyParser);
+app.use(middleware.rawBodyParser);
+app.use(methodOverride());
+
+app.set('port', port);
+app.use(favicon(path.join(buildDir, 'favicon.ico')));
+app.use(express.static(path.join(process.cwd(), 'public')));
+
+app.use((req, res) => {
+  res.sendFile(path.join(buildDir, 'index.html'));
+});
+
+app.listen(port, () => {
+  logger.log('info', `App started and listening at port ${port}`);
+});
+
+process.addListener('uncaughtException', (err) => {
+  logger.error('Uncaught Error', err);
+});
+
+export default app;
